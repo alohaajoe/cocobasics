3d7a2573b0d42ebe9e5da3041a402f8c2f24f26c
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2022-11-04 21:37:37 +0100

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Aloha to my Site',
+  tagline: 'Penguins are cool',
   url: 'https://github.com',
   baseUrl: '/cocobasics/',
   onBrokenLinks: 'throw',
